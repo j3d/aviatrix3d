21341d34235052c267f5bafc5f435f97ddb4be6a
justinc <justinc@buildingiq.com>
2015-05-12 20:10:43 +1000

    Additional unit test coverage

--- aviatrix3d-core/src/test/java/org/j3d/aviatrix3d/management/SingleDisplayCollectionTest.java
+++ aviatrix3d-core/src/test/java/org/j3d/aviatrix3d/management/SingleDisplayCollectionTest.java
@@ -14,16 +14,21 @@
 
 package org.j3d.aviatrix3d.management;
 
+import org.j3d.util.ErrorReporter;
+import org.j3d.util.I18nManager;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
+import org.j3d.aviatrix3d.InvalidWriteTimingException;
 import org.j3d.aviatrix3d.Layer;
 import org.j3d.aviatrix3d.pipeline.RenderableRequestData;
 import org.j3d.aviatrix3d.pipeline.audio.AudioRenderPipeline;
 import org.j3d.aviatrix3d.pipeline.graphics.GraphicsRenderPipeline;
+import org.j3d.aviatrix3d.rendering.LayerCullable;
 
 /**
  * Unit tests for single-threaded rendering.
@@ -32,6 +37,13 @@ import org.j3d.aviatrix3d.pipeline.graphics.GraphicsRenderPipeline;
  */
 public class SingleDisplayCollectionTest
 {
+    @BeforeMethod(groups = "unit")
+    public void setupTests() throws Exception
+    {
+        I18nManager intl_mgr = I18nManager.getManager();
+        intl_mgr.setApplication("TextureUpdateStateManagerTest", "config.i18n.org-j3d-aviatrix3d-resources-core");
+    }
+
     @Test(groups = "unit")
     public void testBasicConstruction() throws Exception
     {
@@ -55,10 +67,48 @@ public class SingleDisplayCollectionTest
         class_under_test.getLayers(fetch_result);
 
         assertSame(test_layers[0], fetch_result[0], "Didn't return our original layer");
+
+        // Now quickly check that putting in invalid arguments doesn't
+        // cause a crash.
+        class_under_test.getLayers(null);
+        class_under_test.getLayers(new Layer[0]);
+    }
+
+    @Test(groups = "unit", dependsOnMethods = "testLayerManagement")
+    public void testAddLayerAfterPipelinesSet() throws Exception
+    {
+        GraphicsRenderPipeline mock_graphics_pipeline = mock(GraphicsRenderPipeline.class);
+        AudioRenderPipeline mock_audio_pipeline = mock(AudioRenderPipeline.class);
+
+        Layer[] test_layers = { new TestLayer() };
+
+        // Use the constructor to set pipelines this time, just to be different.
+        SingleDisplayCollection class_under_test =
+            new SingleDisplayCollection(mock_graphics_pipeline, mock_audio_pipeline);
+        class_under_test.setLayers(test_layers, 1);
+
+        verify(mock_graphics_pipeline, times(1)).setRenderableLayers(test_layers, 1);
+        verify(mock_audio_pipeline, times(1)).setRenderableLayers(test_layers, 1);
+    }
+
+    @Test(groups = "unit", expectedExceptions = IllegalArgumentException.class)
+    public void testLayerAddSizeMismatch() throws Exception
+    {
+        Layer[] test_layers = { new TestLayer() };
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setLayers(test_layers, test_layers.length + 1);
+    }
+
+    @Test(groups = "unit", expectedExceptions = IllegalArgumentException.class)
+    public void testLayerAddNullArray() throws Exception
+    {
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setLayers(null, 1);
     }
 
     @Test(groups = "unit")
-    public void testGraphicsPipelineHandling() throws Exception
+    public void testGraphicsPipelineRendering() throws Exception
     {
         Layer[] test_layers = { new TestLayer() };
 
@@ -73,10 +123,30 @@ public class SingleDisplayCollectionTest
 
         assertTrue(class_under_test.process(), "Did not process anything");
         verify(mock_pipeline, times(1)).setRequestData(any(RenderableRequestData.class));
+        verify(mock_pipeline, times(1)).render();
     }
 
     @Test(groups = "unit")
-    public void testAudioPipelineHandling() throws Exception
+    public void testGraphicsPipelineDisplayOnly() throws Exception
+    {
+        Layer[] test_layers = { new TestLayer() };
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setLayers(test_layers, 1);
+
+        GraphicsRenderPipeline mock_pipeline = mock(GraphicsRenderPipeline.class);
+        when(mock_pipeline.displayOnly()).thenReturn(true);
+
+        class_under_test.addPipeline(mock_pipeline);
+        class_under_test.setEnabled(true);
+
+        assertTrue(class_under_test.displayOnly(), "Did not process anything");
+        verify(mock_pipeline, times(1)).setRequestData(any(RenderableRequestData.class));
+        verify(mock_pipeline, times(1)).displayOnly();
+    }
+
+    @Test(groups = "unit")
+    public void testAudioPipelineRendering() throws Exception
     {
         Layer[] test_layers = { new TestLayer() };
 
@@ -94,6 +164,29 @@ public class SingleDisplayCollectionTest
 
         assertTrue(class_under_test.process(), "Did not process anything");
         verify(mock_pipeline, times(1)).setRequestData(any(RenderableRequestData.class));
+        verify(mock_pipeline, times(1)).render();
+    }
+
+    @Test(groups = "unit")
+    public void testAudioPipelineDisplayOnly() throws Exception
+    {
+        Layer[] test_layers = { new TestLayer() };
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setLayers(test_layers, 1);
+
+        AudioRenderPipeline mock_pipeline = mock(AudioRenderPipeline.class);
+        when(mock_pipeline.displayOnly()).thenReturn(true);
+
+        class_under_test.addPipeline(mock_pipeline);
+
+        // Check the layers got sent....
+
+        class_under_test.setEnabled(true);
+
+        assertTrue(class_under_test.displayOnly(), "Did not process anything");
+        verify(mock_pipeline, times(1)).setRequestData(any(RenderableRequestData.class));
+        verify(mock_pipeline, times(1)).displayOnly();
     }
 
     @Test(groups = "unit")
@@ -119,4 +212,143 @@ public class SingleDisplayCollectionTest
         verify(mock_audio_pipeline, never()).render();
     }
 
+    @Test(groups = "unit", expectedExceptions = InvalidWriteTimingException.class)
+    public void testCantSetLayersWhenLayerWriteDisabled() throws Exception
+    {
+        Layer[] test_layers = { new TestLayer() };
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setEnabled(true);
+        class_under_test.enableLayerChange(false);
+        class_under_test.setLayers(test_layers, 1);
+    }
+
+    @Test(groups = "unit", expectedExceptions = IllegalStateException.class)
+    public void testAddPipelineWhenLayerWriteDisabled() throws Exception
+    {
+        AudioRenderPipeline mock_pipeline = mock(AudioRenderPipeline.class);
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setEnabled(true);
+        class_under_test.enableLayerChange(false);
+        class_under_test.addPipeline(mock_pipeline);
+    }
+
+    @Test(groups = "unit")
+    public void testRemovePipelines() throws Exception
+    {
+        GraphicsRenderPipeline mock_graphics_pipeline = mock(GraphicsRenderPipeline.class);
+        AudioRenderPipeline mock_audio_pipeline = mock(AudioRenderPipeline.class);
+
+        Layer[] test_layers = { new TestLayer() };
+
+        // Use the constructor to set pipelines this time, just to be different.
+        SingleDisplayCollection class_under_test =
+            new SingleDisplayCollection(mock_graphics_pipeline, mock_audio_pipeline);
+        class_under_test.setLayers(test_layers, 1);
+
+        class_under_test.removePipeline(mock_graphics_pipeline);
+
+        verify(mock_graphics_pipeline, times(1)).setRenderableLayers(null, 0);
+        verify(mock_audio_pipeline, never()).setRenderableLayers(null, 0);
+
+        class_under_test.removePipeline(mock_audio_pipeline);
+
+        verify(mock_graphics_pipeline, times(1)).setRenderableLayers(null, 0);
+        verify(mock_audio_pipeline, times(1)).setRenderableLayers(null, 0);
+    }
+
+    @Test(groups = "unit", expectedExceptions = IllegalStateException.class)
+    public void testRemovePipelineWhenLayerWriteDisabled() throws Exception
+    {
+        AudioRenderPipeline mock_pipeline = mock(AudioRenderPipeline.class);
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setEnabled(true);
+        class_under_test.addPipeline(mock_pipeline);
+
+        // Just to make sure that the add really happened and didn't throw the
+        // exception that we are trying to catch.
+        verify(mock_pipeline, times(1)).setRenderableLayers(any(LayerCullable[].class), anyInt());
+
+        // Now change the write state and attempt to remove it
+        class_under_test.enableLayerChange(false);
+        class_under_test.removePipeline(mock_pipeline);
+    }
+
+    @Test(groups = "unit")
+    public void testShutdown() throws Exception
+    {
+        GraphicsRenderPipeline mock_graphics_pipeline = mock(GraphicsRenderPipeline.class);
+        AudioRenderPipeline mock_audio_pipeline = mock(AudioRenderPipeline.class);
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setEnabled(true);
+        class_under_test.addPipeline(mock_graphics_pipeline);
+        class_under_test.addPipeline(mock_audio_pipeline);
+
+        class_under_test.shutdown();
+
+        verify(mock_graphics_pipeline, times(1)).halt();
+        verify(mock_audio_pipeline, times(1)).halt();
+
+        assertTrue(class_under_test.isDisposed(), "A shut down pipeline should be disposed");
+
+        // Call shutdown again and make sure that halt() is not called a
+        // second time on the pipelines. The times() should still be at
+        // 1. If the shutdown went through, it would fail as times()
+        // will have a value of 2.
+
+        class_under_test.shutdown();
+
+        verify(mock_graphics_pipeline, times(1)).halt();
+        verify(mock_audio_pipeline, times(1)).halt();
+    }
+
+    @Test(groups = "unit")
+    public void testHalt() throws Exception
+    {
+        GraphicsRenderPipeline mock_graphics_pipeline = mock(GraphicsRenderPipeline.class);
+        AudioRenderPipeline mock_audio_pipeline = mock(AudioRenderPipeline.class);
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setEnabled(true);
+        class_under_test.addPipeline(mock_graphics_pipeline);
+        class_under_test.addPipeline(mock_audio_pipeline);
+
+        class_under_test.halt();
+
+        verify(mock_graphics_pipeline, times(1)).halt();
+        verify(mock_audio_pipeline, times(1)).halt();
+
+        assertFalse(class_under_test.isDisposed(), "Halt should not dispose the handler");
+
+        // Call halt again and make sure that halt() is called a
+        // second time on the pipelines. There should be no filtering
+        // on the current state of the rest of the class
+
+        class_under_test.halt();
+
+        verify(mock_graphics_pipeline, times(2)).halt();
+        verify(mock_audio_pipeline, times(2)).halt();
+    }
+
+    @Test(groups = "unit")
+    public void testErrorReporterPropagation() throws Exception
+    {
+        GraphicsRenderPipeline mock_graphics_pipeline = mock(GraphicsRenderPipeline.class);
+        AudioRenderPipeline mock_audio_pipeline = mock(AudioRenderPipeline.class);
+
+        ErrorReporter test_reporter = mock(ErrorReporter.class);
+
+        SingleDisplayCollection class_under_test = new SingleDisplayCollection();
+        class_under_test.setEnabled(true);
+        class_under_test.addPipeline(mock_graphics_pipeline);
+        class_under_test.addPipeline(mock_audio_pipeline);
+
+        class_under_test.setErrorReporter(test_reporter);
+
+        verify(mock_graphics_pipeline, times(1)).setErrorReporter(test_reporter);
+        verify(mock_audio_pipeline, times(1)).setErrorReporter(test_reporter);
+    }
 }
