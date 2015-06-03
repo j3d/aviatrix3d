5c2850c4308594d27a6322674f856a754fa0a2ae
justinc <justinc@buildingiq.com>
2015-06-04 08:25:05 +1000

    Tweak to the unit tests to make them more generic and cover more cases.

--- aviatrix3d-core/src/test/java/org/j3d/aviatrix3d/management/DefaultPickingHandlerTest.java
+++ aviatrix3d-core/src/test/java/org/j3d/aviatrix3d/management/DefaultPickingHandlerTest.java
@@ -69,15 +69,30 @@ public class DefaultPickingHandlerTest
         verifyNoMoreInteractions(mock_target);
     }
 
-    // ------- Point Picking Tests -------------------------------------------
-
-    @Test(groups = "unit", dataProvider = "single child pick options")
-    public void testPointSinglePick(int sortType, int pickType) throws Exception
+    @Test(groups = "unit", dataProvider = "pick options")
+    public void testAllSinglePick(int geometryType, float[] origin, float[] destination, float additionalData,
+                                  int sortType, int pickType) throws Exception
     {
         PickRequest test_request = new PickRequest();
-        test_request.pickGeometryType = PickRequest.PICK_POINT;
+        test_request.pickGeometryType = geometryType;
         test_request.pickSortType = sortType;
         test_request.pickType = pickType;
+
+        if(origin != null)
+        {
+            test_request.origin[0] = origin[0];
+            test_request.origin[1] = origin[1];
+            test_request.origin[2] = origin[2];
+        }
+
+        if(destination != null)
+        {
+            test_request.destination[0] = destination[0];
+            test_request.destination[1] = destination[1];
+            test_request.destination[2] = destination[2];
+        }
+
+        test_request.additionalData = additionalData;
 
         PickTarget mock_child = mock(LeafPickTarget.class);
         when(mock_child.checkPickMask(pickType)).thenReturn(true);
@@ -106,6 +121,9 @@ public class DefaultPickingHandlerTest
 //        assertEquals(result_path.getNodeCount(), 1, "Wrong number of nodes in path");
 //        assertEquals(result_path.getNode(0), mock_target, "Target not in the path");
     }
+
+
+    // ------- Point Picking Tests -------------------------------------------
 
     @Test(groups = "unit")
     public void testPointSinglePickNoChildren() throws Exception
@@ -221,43 +239,6 @@ public class DefaultPickingHandlerTest
 
     // ------  Ray Picking Tests ---------------------------------------------
 
-    @Test(groups = "unit", dataProvider = "single child pick options")
-    public void testRaySinglePick(int sortType, int pickType) throws Exception
-    {
-        PickRequest test_request = new PickRequest();
-        test_request.pickGeometryType = PickRequest.PICK_RAY;
-        test_request.pickSortType = sortType;
-        test_request.pickType = pickType;
-        test_request.destination[1] = 1.0f;
-
-        PickTarget mock_child = mock(LeafPickTarget.class);
-        when(mock_child.checkPickMask(pickType)).thenReturn(true);
-        when(mock_child.getPickTargetType()).thenReturn(PickTarget.LEAF_PICK_TYPE);
-        when(mock_child.getPickableBounds()).thenReturn(new BoundingBox());
-
-        SinglePickTarget mock_target = mock(SinglePickTarget.class);
-        when(mock_target.checkPickMask(pickType)).thenReturn(true);
-        when(mock_target.getPickTargetType()).thenReturn(PickTarget.SINGLE_PICK_TYPE);
-        when(mock_target.getPickableBounds()).thenReturn(new BoundingBox());
-        when(mock_target.getPickableChild()).thenReturn(mock_child);
-
-        DefaultPickingHandler class_under_test = new DefaultPickingHandler();
-        class_under_test.pickSingle(mock_target, test_request);
-
-        assertEquals(test_request.pickCount, 1, "Expected an intersection");
-
-        if(sortType == PickRequest.SORT_ALL || sortType == PickRequest.SORT_ORDERED)
-            assertTrue(test_request.foundPaths instanceof List, "Bulk sort should return a list of paths");
-        else
-            assertTrue(test_request.foundPaths instanceof SceneGraphPath, "Single sort should return a path");
-
-        // Can't test this as the mock does not implement both PickTarget and Node, so the
-        // check in the SceneGraphPath update() method will ignore the mocked object.
-//        SceneGraphPath result_path = (SceneGraphPath)test_request.foundPaths;
-//        assertEquals(result_path.getNodeCount(), 1, "Wrong number of nodes in path");
-//        assertEquals(result_path.getNode(0), mock_target, "Target not in the path");
-    }
-
     @Test(groups = "unit")
     public void testRayPickNoIntersection() throws Exception
     {
@@ -347,43 +328,6 @@ public class DefaultPickingHandlerTest
     }
 
     // ------  Line Segment Picking Tests ------------------------------------
-
-    @Test(groups = "unit", dataProvider = "single child pick options")
-    public void testLineSegmentSinglePick(int sortType, int pickType) throws Exception
-    {
-        PickRequest test_request = new PickRequest();
-        test_request.pickGeometryType = PickRequest.PICK_LINE_SEGMENT;
-        test_request.pickSortType = sortType;
-        test_request.pickType = pickType;
-        test_request.destination[1] = 1.0f;
-
-        PickTarget mock_child = mock(LeafPickTarget.class);
-        when(mock_child.checkPickMask(pickType)).thenReturn(true);
-        when(mock_child.getPickTargetType()).thenReturn(PickTarget.LEAF_PICK_TYPE);
-        when(mock_child.getPickableBounds()).thenReturn(new BoundingBox());
-
-        SinglePickTarget mock_target = mock(SinglePickTarget.class);
-        when(mock_target.checkPickMask(pickType)).thenReturn(true);
-        when(mock_target.getPickTargetType()).thenReturn(PickTarget.SINGLE_PICK_TYPE);
-        when(mock_target.getPickableBounds()).thenReturn(new BoundingBox());
-        when(mock_target.getPickableChild()).thenReturn(mock_child);
-
-        DefaultPickingHandler class_under_test = new DefaultPickingHandler();
-        class_under_test.pickSingle(mock_target, test_request);
-
-        assertEquals(test_request.pickCount, 1, "Expected an intersection");
-
-        if(sortType == PickRequest.SORT_ALL || sortType == PickRequest.SORT_ORDERED)
-            assertTrue(test_request.foundPaths instanceof List, "Bulk sort should return a list of paths");
-        else
-            assertTrue(test_request.foundPaths instanceof SceneGraphPath, "Single sort should return a path");
-
-        // Can't test this as the mock does not implement both PickTarget and Node, so the
-        // check in the SceneGraphPath update() method will ignore the mocked object.
-//        SceneGraphPath result_path = (SceneGraphPath)test_request.foundPaths;
-//        assertEquals(result_path.getNodeCount(), 1, "Wrong number of nodes in path");
-//        assertEquals(result_path.getNode(0), mock_target, "Target not in the path");
-    }
 
     @Test(groups = "unit")
     public void testLineSegmentPickNoIntersection() throws Exception
@@ -518,4 +462,84 @@ public class DefaultPickingHandlerTest
             { SinglePickTarget.class, PickTarget.SINGLE_PICK_TYPE }
         };
     }
+
+    @DataProvider(name = "pick options")
+    public Object[][] generatePickComboOptions()
+    {
+        float[] direction_up = { 0, 1, 0 };
+        return new Object[][]
+        {
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_ALL, PickRequest.FIND_ALL },
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_ANY, PickRequest.FIND_ALL },
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_ALL },
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_ALL },
+
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_ALL, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_ANY, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_POINT, null, null, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_GENERAL },
+
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_ALL, PickRequest.FIND_ALL },
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_ANY, PickRequest.FIND_ALL },
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_ALL },
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_ALL },
+
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_ALL, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_ANY, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_LINE_SEGMENT, null, direction_up, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_GENERAL },
+
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_ALL, PickRequest.FIND_ALL },
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_ANY, PickRequest.FIND_ALL },
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_ALL },
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_ALL },
+
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_ALL, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_ANY, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_RAY, null, direction_up, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_GENERAL },
+
+// JC: Disabled because BoundingSphere and BoundingBox don't implement the checkIntersectionCylinder yet
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_ALL, PickRequest.FIND_ALL },
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_ANY, PickRequest.FIND_ALL },
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_CLOSEST, PickRequest.FIND_ALL },
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_ORDERED, PickRequest.FIND_ALL },
+//
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_ALL, PickRequest.FIND_GENERAL },
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_ANY, PickRequest.FIND_GENERAL },
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_CLOSEST, PickRequest.FIND_GENERAL },
+//            { PickRequest.PICK_CYLINDER, null, direction_up, 1, PickRequest.SORT_ORDERED, PickRequest.FIND_GENERAL },
+
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_ALL, PickRequest.FIND_ALL },
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_ANY, PickRequest.FIND_ALL },
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_CLOSEST, PickRequest.FIND_ALL },
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_ORDERED, PickRequest.FIND_ALL },
+//
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_ALL, PickRequest.FIND_GENERAL },
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_ANY, PickRequest.FIND_GENERAL },
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_CLOSEST, PickRequest.FIND_GENERAL },
+//            { PickRequest.PICK_CONE, null, direction_up, 1, PickRequest.SORT_ORDERED, PickRequest.FIND_GENERAL },
+
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_ALL, PickRequest.FIND_ALL },
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_ANY, PickRequest.FIND_ALL },
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_ALL },
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_ALL },
+
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_ALL, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_ANY, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_CLOSEST, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_BOX, null, direction_up, 0, PickRequest.SORT_ORDERED, PickRequest.FIND_GENERAL },
+
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_ALL, PickRequest.FIND_ALL },
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_ANY, PickRequest.FIND_ALL },
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_CLOSEST, PickRequest.FIND_ALL },
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_ORDERED, PickRequest.FIND_ALL },
+
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_ALL, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_ANY, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_CLOSEST, PickRequest.FIND_GENERAL },
+            { PickRequest.PICK_SPHERE, null, null, 0.5f, PickRequest.SORT_ORDERED, PickRequest.FIND_GENERAL },
+        };
+    }
+
 }
