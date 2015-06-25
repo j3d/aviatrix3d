320cff2e4c07a1051dedaf6aa9dd650879bd344c
justinc <justinc@buildingiq.com>
2015-06-26 08:38:06 +1000

    Tests for ray and segment intersections.

--- aviatrix3d-core/src/test/java/org/j3d/aviatrix3d/BoundingSphereTest.java
+++ aviatrix3d-core/src/test/java/org/j3d/aviatrix3d/BoundingSphereTest.java
@@ -86,6 +86,20 @@ public class BoundingSphereTest
                      "Ray intersection incorrect");
     }
 
+    @Test(groups = "unit", dataProvider = "segment tests")
+    public void testLineSegmentIntersection(float[] centre,
+                                            float radius,
+                                            float[] testStart,
+                                            float[] testEnd,
+                                            boolean expectedResult)
+    {
+        BoundingSphere class_under_test = new BoundingSphere(centre, radius);
+
+        assertEquals(class_under_test.checkIntersectionSegment(testStart, testEnd),
+                     expectedResult,
+                     "Ray intersection incorrect");
+    }
+
     @DataProvider(name = "point tests")
     public Object[][] generatePointTestsData()
     {
@@ -117,7 +131,7 @@ public class BoundingSphereTest
     @DataProvider(name = "ray tests")
     public Object[][] generateRayTestsData()
     {
-        Object[][] ret_val = new Object[7][5];
+        Object[][] ret_val = new Object[8][5];
 
         // Ray starts inside sphere, pointing out
         ret_val[0][0] = new float[] { 0, 0, 0 };
@@ -162,6 +176,63 @@ public class BoundingSphereTest
         ret_val[6][2] = new float[] { -3.0f, 0.0f, 0.0f };
         ret_val[6][3] = new float[] {  0.0f, 0.0f, 1.0f };
         ret_val[6][4] = false;
+
+        ret_val[7][0] = new float[] { 0, 0, 0 };
+        ret_val[7][1] = 1;
+        ret_val[7][2] = new float[] { 1.5f, 0.0f, 0.0f };
+        ret_val[7][3] = new float[] { 0.0f, 1.0f, 0.0f };
+        ret_val[7][4] = false;
+
+        return ret_val;
+    }
+
+    @DataProvider(name = "segment tests")
+    public Object[][] generateSegmentTestsData()
+    {
+        // Segment and box are almost identical in tests
+        Object[][] ret_val = new Object[6][5];
+
+        // segment completely contained
+        ret_val[0][0] = new float[] { 0, 0, 0 };
+        ret_val[0][1] = 1;
+        ret_val[0][2] = new float[] { -0.5f, -0.5f, -0.5f };
+        ret_val[0][3] = new float[] {  0.5f,  0.5f,  0.5f };
+        ret_val[0][4] = true;
+
+        // Segment completely outside but passing through
+        ret_val[1][0] = new float[] { 0, 0, 0 };
+        ret_val[1][1] = 1;
+        ret_val[1][2] = new float[] { -1.5f, -1.5f, -1.5f };
+        ret_val[1][3] = new float[] {  1.5f,  1.5f,  1.5f };
+        ret_val[1][4] = true;
+
+        // Segment partial - positive
+        ret_val[2][0] = new float[] { 0, 0, 0 };
+        ret_val[2][1] = 1;
+        ret_val[2][2] = new float[] {  0.0f,  0.0f,  0.0f };
+        ret_val[2][3] = new float[] {  1.5f,  1.5f,  1.5f };
+        ret_val[2][4] = true;
+
+        // Segment partial - negative
+        ret_val[3][0] = new float[] { 0, 0, 0 };
+        ret_val[3][1] = 1;
+        ret_val[3][2] = new float[] { -1.5f, -1.5f, -1.5f };
+        ret_val[3][3] = new float[] {  0.0f,  0.0f,  0.0f };
+        ret_val[3][4] = true;
+
+        // No overlap - negative
+        ret_val[4][0] = new float[] { 0, 0, 0 };
+        ret_val[4][1] = 1;
+        ret_val[4][2] = new float[] { -1.5f, -1.5f, -1.5f };
+        ret_val[4][3] = new float[] { -1.25f,-1.25f,-1.25f };
+        ret_val[4][4] = false;
+
+        // No overlap - positive
+        ret_val[5][0] = new float[] { 0, 0, 0 };
+        ret_val[5][1] = 1;
+        ret_val[5][2] = new float[] {  1.25f, 1.25f, 1.25f };
+        ret_val[5][3] = new float[] {  1.5f,  1.5f,  1.5f };
+        ret_val[5][4] = false;
 
         return ret_val;
     }
